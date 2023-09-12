14f6b3217282fd8293639d5e7a71f8f087a0f57b
ShiGu <115810131+bengs17@users.noreply.github.com>
2023-09-12 17:26:11 -0400

    Update exercise04.py

--- assignment/exercise04.py
+++ assignment/exercise04.py
@@ -4,14 +4,38 @@ Use analog input with photocell
 
 import time
 import machine
+import os
+import json 
 
+def get_params(param_file: str) -> dict:
+    """Reads parameters from a JSON file."""
+
+    if not is_regular_file(param_file):
+        raise OSError(f"File {param_file} not found")
+
+    with open(param_file) as f:
+        params = json.load(f)
+
+    return params
+
+def is_regular_file(path: str) -> bool:
+    """Checks if a regular file exists."""
+
+    S_IFREG = 0x8000
+
+    try:
+        return os.stat(path)[0] & S_IFREG != 0
+    except OSError:
+        return False
+    
 led = machine.Pin("LED", machine.Pin.OUT)
 adc = machine.ADC(28)
 
 blink_period = 0.1
 
-max_bright = 20000
-min_bright = 10000
+params = get_params("exercise04.json")
+max_bright = params["max_bright"]
+min_bright = params["min_bright"]
 
 while True:
     value = adc.read_u16()
@@ -21,3 +45,4 @@ while True:
     time.sleep(blink_period * duty_cycle)
     led.low()
     time.sleep(blink_period * (1 - duty_cycle))
+
